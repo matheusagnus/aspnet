e695428ea2f29faf9fc9499184a1c31b2e828031
matheusagnus <ferreira.zx@gmail.com>
2018-12-17 10:00:25 -0200

    first

---
+++ Context/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using ProjetoWebLP4.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoWebLP4.Context
+{
+    public class Contexto : DbContext
+    {
+        public Contexto()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+
+}
+
---
+++ Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoWebLP4.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; } 
+        public int Matricula { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+    }
+}
